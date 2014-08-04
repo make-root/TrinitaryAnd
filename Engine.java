9751a45879040017674ebfa7baccfa7429bf7915
mk_root <make.root@gmail.com>
2014-08-04 18:36:28 +0300

    Its begin to work but some streandly

--- Engine.java
+++ Engine.java
@@ -1,24 +1,32 @@
 import java.awt.event.ActionListener;  
-import java.awt.event.ActionEvent; 
-//import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
  
 public class Engine implements ActionListener {
     
-    GUI parent;//ССілка на гуи
+    GUI parent;//ССлка на гуи
     Engine (GUI parent){
         this.parent=parent;
     }
     
    public void actionPerformed(ActionEvent e){ 
        JButton clickedButton = (JButton) e.getSource();//источник действия
-       
+
+//       System.out.println("in engine");
+
        String FieldText1=parent.InputField1.getText();
        String FieldText2=parent.InputField2.getText();
        String FieldText3=parent.InputField3.getText();
        String FieldText4=parent.InputField4.getText();
        String FieldText5=parent.InputField5.getText();
        String FieldText6=parent.InputField6.getText();
+
+//       System.out.println(FieldText1);
+//       System.out.println(FieldText2);
+//       System.out.println(FieldText3);
+//       System.out.println(FieldText4);
+//       System.out.println(FieldText5);
+//       System.out.println(FieldText6);
        
        double Field1=0;
        double Field2=0;
@@ -28,7 +36,7 @@ public class Engine implements ActionListener {
        double Field6=0;
        
        if (!"".equals(FieldText1)){
-           Field1= Double.parseDouble(FieldText1);
+           Field1= (Double.parseDouble(FieldText1)/100);
         }
        if (!"".equals(FieldText2)){
            Field2= Double.parseDouble(FieldText2);
@@ -40,35 +48,42 @@ public class Engine implements ActionListener {
            Field4= Double.parseDouble(FieldText4);
         }
         if (!"".equals(FieldText5)){
-           Field5= Double.parseDouble(FieldText5);
+           Field5= Double.parseDouble(FieldText5)/100;
         }
         if (!"".equals(FieldText6)){
            Field6= Double.parseDouble(FieldText6);
         }
         
-        double Cell1;
-        double Cell2;
-        double Cell3;
-        double Cell4;
-        double Cell5;
-        double Cell6;
-        double Cell7;
-        double Cell8;
-        double Cell9;
+
         
-        Cell1=Field1*Field4;
-        Cell2=Field2*Field4;
-        Cell3=Field3*Field4;
-        
-        Cell4=Field1*Field5;
-        Cell5=Field2*Field5;
-        Cell6=Field3*Field5;
-        
-        Cell7=Field1*Field6;
-        Cell8=Field2*Field6;
-        Cell9=Field3*Field6;
-        
-        //parent.Label1.setText(Cell1);
-        
+        double Cell1=Field1*Field4;
+        double Cell2=Field2*Field4;
+        double Cell3=Field3*Field4;
+
+        double Cell4=Field1*Field5;
+        double Cell5=Field2*Field5;
+        double Cell6=Field3*Field5;
+
+        double Cell7=Field1*Field6;
+        double Cell8=Field2*Field6;
+        double Cell9=Field3*Field6;
+
+        double Cell10=Cell1;
+        double Cell11=Cell2+Cell4+Cell5;
+        double Cell12=Cell3+Cell6+Cell7+Cell8+Cell9;
+
+        parent.Label1.setText(Double.toString(Cell1));
+        parent.Label2.setText(Double.toString(Cell2));
+        parent.Label3.setText(Double.toString(Cell3));
+        parent.Label4.setText(Double.toString(Cell4));
+        parent.Label5.setText(Double.toString(Cell5));
+        parent.Label6.setText(Double.toString(Cell6));
+        parent.Label7.setText(Double.toString(Cell7));
+        parent.Label8.setText(Double.toString(Cell8));
+        parent.Label9.setText(Double.toString(Cell9));
+
+        parent.Label10.setText(Double.toString(Cell10));
+        parent.Label11.setText(Double.toString(Cell11));
+        parent.Label12.setText(Double.toString(Cell12));
    } 
 }
--- Trinitary_AND.java
+++ Trinitary_AND.java
@@ -1,4 +1,4 @@
-class Trinitary_AND
+class Trinitary_And
 {public static void main(String[] args)
 // Создаём панель
     {GUI gui=new GUI();
